5c59274fc3aafde4c290a4f08439b188eb8c6df2
zmamedova21316 <zmamedova21316@ada.edu.az>
2026-02-06 13:33:28 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(s: "Enter first number:");
+
+    }
+}
